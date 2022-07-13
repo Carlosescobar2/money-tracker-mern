2fd833e336350e3cfb0c4604ba57806793700a7d
Carlos Escobar <escobarcarlos@gmail.com>
2022-07-13 14:52:57 -0700

    added-useforms

---
+++ src/components/Forms.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {useForm} from 'react-hook-form'
+
+const Forms = () => {
+    const[register, handleSubmit, resetField]=useForm();
+
+    const onSubmit = (data)=> { 
+        console.log(data)
+    }
+
+  return (
+    <div className='form max-w-sm mx-auto w-96'>
+        <h1 className='font-bold pb-4 text-xl'>Transaction</h1>
+
+        <form id="form" onSubmit={handleSubmit(onSubmit)} action="">
+            <div className='grid gap-4'>
+                <div className='input-group'>
+                    <input type="text" {...register('name')} placeholder='Sallary, House Rent, SIP' className='form-input' />
+                </div>
+                <select className='form-input' {...register('name')}>
+                    <option value="Investment" defaultValue>Investment</option>
+                    <option value="Expense" defaultValue>Expense</option>
+                    <option value="Savings" defaultValue>Savings</option>
+
+                </select>
+                <div className='input-group'>
+                    <input type="text" {...register('amount')} placeholder='Amount' className='form-input' />
+                </div>
+                <div className='submit-btn'>
+                    <button className='border py-2 text-white bg-indigo-500 w-full'>Make Transaction</button>
+                </div>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Forms
