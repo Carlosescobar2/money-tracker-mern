1e5c589dec15c859c7b0076a4820de2f5f34727e
Carlos Escobar <escobarcarlos@gmail.com>
2022-07-17 19:41:43 -0700

    express-insert

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import 'boxicons'
 const obj = [
     { 
         name: "Savings",
@@ -30,7 +30,8 @@ function Transaction({catergory}){
     if(!catergory) return null;
     return( 
         <div className='item flex justify-center bg-gray-50 py-2 rounded-r' style={{borderRight: `8px solid ${catergory.color ?? "#e5e5e5"}`}}>
-            <span className='block w-full'>{catergory.name ?? ''}</span>
+            <button className='px-3'><box-icon color={catergory.color ?? ''} size="15px" name="trash"></box-icon></button>
+            <span className='block w-full'>{catergory.name ?? '#e5e5e5'}</span>
         </div>
     )
 }
